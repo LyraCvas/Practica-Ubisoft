bb0b06d31ae4b09917087c0404c83259fa23a28b
Sebastian Lira <sebastianandresla16@gmail.com>
2024-08-28 00:53:19 -0400

    ultimas noticias

---
+++ js/articulos.js
@@ -0,0 +1,37 @@
+const articulos = [
+    {
+        imagen:"https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/4t5GesE6GU0rcNvBPSS3wn/63a8515b5120b5784fe4830df207d732/XD_DirectRelief.jpg",
+        linkNoti:"https://news.ubisoft.com/es-mx/article/6crMbGTfPXrH2LsMKOU3cq/xdefiant-direct-relief-charity-bundle-available-until-sept-2",
+        titulo:"XDefiant Direct Relief Charity Bundle Available Until Sept. 2",
+        fecha:"26 agosto 2024",
+        juego:"XDefiand"
+    },    
+    {
+        imagen:"https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/7I4u8DfLe8AYUQOTd22JdH/3d8cd85684ebaa42c3eb53823f2890e9/Y9S3_Operator_Keyart.jpg",
+        linkNoti:"https://news.ubisoft.com/es-mx/article/6md8NyzcybEjZ2bMROA5fP/rainbow-six-siege--operation-twin-shells-operator-and-gadget-guide",
+        titulo:"Rainbow Six Siege – Operation Twin Shells Operator and Gadget Guide",
+        fecha:"26 agosto 2024",
+        juego:"Rainbow Six Siege"
+    },
+    {
+        imagen:"https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/4KcKBmyRfrsl2lCqtOFB8t/03f4c6351dd932ea035a5632ae606348/HoMM_OE_KV_2_logo.jpg",
+        linkNoti:"https://news.ubisoft.com/es-mx/article/62HpoWQkAIVogVkoVk8JuJ/heroes-of-might-and-magic-olden-era-unveiled-at-gamescom",
+        titulo:"Heroes of Might and Magic: Olden Era Unveiled at Gamescom",
+        fecha:"22 agosto 2024",
+        juego:"Heroes of Might and Magic"
+    },
+    {
+        imagen:"https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/l0KH8SzN01pX6JCxPD680/6118e909fff80da2e88086115634c3e8/SkullAndBones_Steam.jpg",
+        linkNoti:"https://news.ubisoft.com/es-mx/article/1r70KgZtUkbbl22aPup39j/skull-and-bones-season-3-into-the-dragons-wake-out-now",
+        titulo:"Skull and Bones Season 3, Into the Dragon’s Wake, Out Now",
+        fecha:"22 agosto 2024",
+        juego:"Skull and Bones"
+    },
+    {
+        imagen:"https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6epHN5JzqHnwK1SXZCuAmX/bca6a00854dbd2dc4582f7712512c0e9/un-swo-arcadecabinets.jpg",
+        linkNoti:"https://news.ubisoft.com/es-mx/article/40ih01hW2jAmPLnWeZnCuw/how-the-star-wars-outlaws-team-created-playable-arcade-games",
+        titulo:"How the Star Wars Outlaws™ Team Created Playable Arcade Games",
+        fecha:"20 agosto 2024",
+        juego:"Star Wars Outlaws"
+    }
+]
